fb06f020145b97bb3b849b0419207b3ee5d7f7c9
BtramVu <bbbvut@gmail.com>
2020-12-24 03:20:13 -0800

    create 1/9 line for time block and change css style

---
+++ Assets/script.js
@@ -0,0 +1,22 @@
+// get live time and date. Luxon? 
+// name var for the user input that will be save in the schedule 
+// use local storage to getItem in this case user input, JSON it and then Parse it
+// on click function for save button. excute the save? putting the user input text into the line of hour
+// for loop? for var =0 for loop, each input = var[i]
+// pushing real life hour into the time block ...
+// conditional statement, if present time < futuretime change color? past hour value <present time change color
+
+
+var todoSchedule = {
+
+}
+
+// local storage to store that schedule input 
+// localStorage.getItem("todoSchedule")
+// localStorage.setItem ("todoSchedule")
+
+// need this here to make sure whatever you do, it is ready 
+// $(document).ready(function () {}
+// basic code: 
+// $().on("Click", function (){})
+
